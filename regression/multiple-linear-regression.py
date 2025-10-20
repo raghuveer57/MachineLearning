76b306b59c1f44a3a33ec9ea188a48614f3cadcc
Raghuveer Ch S <raghuveerchs@gmail.com>
2025-10-20 08:56:16 +0530

    Implement multiple linear regression model with categorical variable encoding and prediction output

--- regression/multiple-linear-regression.py
+++ regression/multiple-linear-regression.py
@@ -16,7 +16,99 @@ y = dataset.iloc[:, -1].values # last column
 print("Read the data successfully")
 
 # displaying the data (optional)
-print(X)
-print(y)
+# print(X)
+# print(y)
 ##################################################
 ##################################################
+
+
+
+
+
+
+
+
+
+
+
+#Encoding categorical variables.##################
+
+#Encoding independent variable X
+##################################################
+##################################################
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+# Create the column transformer
+ct = ColumnTransformer(
+    transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+
+# Fit and transform the data
+X = np.array(ct.fit_transform(X)) # force the ouput into a numpy array
+
+# print(X)
+##################################################
+##################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Splitting the dataset into the Training set and Test set
+##################################################
+##################################################
+from sklearn.model_selection import train_test_split
+
+# Splitting the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# print the new data sets
+# print("X_train:")
+# print(X_train)
+# print("X_test:")
+# print(X_test)
+# print("y_train:")
+# print(y_train)
+# print("y_test:")
+# print(y_test)
+##################################################
+##################################################
+
+
+
+
+
+
+#  Training the multiple linear regression model
+##################################################
+##################################################
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) # fit the model to the training data
+print("Model trained successfully")
+
+##################################################
+##################################################
+
+
+
+ 
+
+# Predicting the Test set results
+##################################################
+##################################################
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2) # set precision for printing
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1)) # compare predicted and actual values
+
+##################################################
+##################################################  
